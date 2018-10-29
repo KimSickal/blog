c9e783e283abe813270095a709f068b1a926d80c
KimSickal <shihwan1004@naver.com>
2018-10-29 12:56:40 +0900

    add module to read .md files

--- src/v1/components/ArtworkComponent.tsx
+++ src/v1/components/ArtworkComponent.tsx
@@ -1,20 +1,38 @@
 import * as React from 'react';
+import * as ReactMarkdown from 'react-markdown';
 
-interface ComponentProps{
-  image: string;
+interface ComponentProps {
+  image: string[];
+  markdownAddress: string;
+}
+
+interface ComponentState {
   text: string;
 }
 
-export class ArtworkComponent extends React.Component<ComponentProps> {
+export class ArtworkComponent extends React.Component<ComponentProps, ComponentState> {
+  constructor(props: ComponentProps) {
+    super(props)
+    console.log(require('../../data/text/README.md'));
+    this.state = { text: "" };
+  }
+  componentDidMount(){
+    fetch(require('../../data/text/README.md')).then((response) => {
+      return response.text().then((markdownText) => {
+        console.log(markdownText)
+        this.setState({ text: markdownText });
+      })
+    })
+  }
+  
   public render() {
     const {
       image,
-      text,
     } = this.props;
     return (
       <div className='Artwork'>
-        <img src={require('../../data/image/' + image)} />
-        <p>{text}</p>
+        <img src={require(`../../data/image/${image[0]}`)} />
+        <ReactMarkdown source={this.state.text} />
       </div>
     );
   }
