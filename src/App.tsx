0678b1de4faa9fb98b4de4adae018fd190da23d9
KimSickal <shihwan1004@naver.com>
2018-11-02 17:31:48 +0900

    add version controller

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import './App.css';
 import { ImagePost } from './v1/models/Posts';
 import { V1Container } from './v1/V1Container';
+import { V2Container } from './v2/V2Container';
 
 interface ComponentStates {
   data: ImagePost[];
   screenWidth: number;
+  selectedVersion: string;
 }
 
 class App extends React.Component<{}, ComponentStates> {
@@ -14,7 +16,11 @@ class App extends React.Component<{}, ComponentStates> {
     this.state = {
       data: [],
       screenWidth: Math.max(Math.min(document.body.clientWidth, 800), 400),
+      selectedVersion: 'v1',
     };
+  }
+  handleSelect(event: React.FormEvent<HTMLSelectElement>) {
+    this.setState({ selectedVersion: event.currentTarget.value })
   }
   componentDidMount() {
     this.setState({
@@ -22,11 +28,27 @@ class App extends React.Component<{}, ComponentStates> {
     })
     window.addEventListener('resize', () => { this.setState({ screenWidth: document.body.clientWidth }) })
   }
+
   public render() {
     let maxWidth = Math.max(Math.min(document.body.clientWidth, 800), 400);
     return (
       <div className="App" style={{ width: maxWidth }}>
-        <V1Container data={this.state.data} />
+        <select className="versionList" onChange={event => this.handleSelect(event)} value={this.state.selectedVersion}>
+          <option value='v1'>v1</option>
+          <option value='v2'>v2</option>
+        </select>
+        {
+          (() => {
+            switch (this.state.selectedVersion) {
+              case 'v1':
+                return <V1Container data={this.state.data} />;
+              case 'v2':
+                return <V2Container data={this.state.data} />;
+              default:
+                return null;
+            }
+          })()
+        }
       </div>
     );
   }
---
+++ src/v2/components/ArtworkComponent.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import * as ReactMarkdown from 'react-markdown';
+import '../styles/ArtworkComponent.css';
+
+interface ComponentProps {
+  images: string[];
+  markdownAddress: string;
+  title: string;
+}
+
+interface ComponentState {
+  text: string;
+  isOpen: boolean;
+}
+
+export class ArtworkComponent extends React.Component<ComponentProps, ComponentState> {
+  constructor(props: ComponentProps) {
+    super(props)
+    this.state = {
+      text: "",
+      isOpen: false,
+    };
+  }
+
+  private loadMarkdown() {
+    fetch(require('../../data/text/README.md')).then((response) => {
+      return response.text().then((markdownText) => {
+        this.setState({ text: markdownText });
+      })
+    })
+  }
+
+  public render() {
+    const {
+      images,
+      title,
+    } = this.props;
+    return (
+      <div
+        className='Artwork'
+      >
+        <div
+          className='Title'
+          onClick={() => {
+            this.setState({ isOpen: !this.state.isOpen });
+            if (!this.state.isOpen) {
+              this.loadMarkdown();
+            }
+          }}
+        >
+          <h1>
+            {title}
+          </h1>
+          <h1>
+            {this.state.isOpen ? '▲' : '▼'}
+          </h1>
+        </div>
+        {this.state.isOpen ?
+          <div>
+            <div
+              className='Content'
+            >
+              {
+                images.map((image, i) => {
+                  return <img src={require(`../../data/image/${image}`)} key={i} />
+                })
+              }
+            </div>
+            <div
+              className='Markdown'
+            >
+              <ReactMarkdown source={this.state.text} />
+            </div>
+          </div>
+          :
+          null
+        }
+      </div>
+    );
+  }
+}
