55fdfcf7ca81d5a5806ce39129f821c66b2a6c20
KimSickal <shihwan1004@naver.com>
2019-01-07 12:00:32 +0900

    갤러리 infinite scroll 적용

--- src/constants/screen.ts
+++ src/constants/screen.ts
@@ -2,3 +2,5 @@ export const screenSize = {
 	maxWidth: 1000,
 	minWidth: 400,
 };
+
+export const artworkForScreen = 3;
--- src/v4/containers/GalleryContainer.tsx
+++ src/v4/containers/GalleryContainer.tsx
@@ -13,6 +13,10 @@ import {
 } from '../components';
 
 import {
+	artworkForScreen,
+} from 'src/constants';
+
+import {
 	styles,
 } from '../styles/GalleryContainerStyle';
 
@@ -21,7 +25,42 @@ interface ComponentProps {
 	match: match;
 }
 
-export class GalleryContainer extends React.Component<ComponentProps & RouterProps> {
+interface ComponentStates {
+	lengthOfList: number;
+}
+
+export class GalleryContainer extends React.Component<ComponentProps & RouterProps, ComponentStates> {
+	constructor(props: ComponentProps & RouterProps) {
+		super(props);
+		this.state = {
+			lengthOfList: Math.min(this.props.data.length, artworkForScreen),
+		};
+		this.onScroll = this.onScroll.bind(this);
+	}
+
+	public componentDidMount() {
+		window.onscroll = this.onScroll;
+	}
+
+	private onScroll() {
+		const {
+			data,
+		} = this.props;
+
+		const {
+			lengthOfList,
+		} = this.state;
+
+		console.log(`${window.innerHeight} + ${document.documentElement.scrollTop} = ${window.innerHeight + document.documentElement.scrollTop}`);
+		console.log(`${document.documentElement.offsetHeight}`);
+
+		if(window.innerHeight + document.documentElement.scrollTop + 500 >= document.documentElement.offsetHeight) {
+			this.setState({
+				lengthOfList: Math.min(lengthOfList + artworkForScreen, data.length),
+			});
+		}
+	}
+
 	public render() {
 		const {
 			data,
@@ -29,12 +68,16 @@ export class GalleryContainer extends React.Component<ComponentProps & RouterPro
 			history,
 		} = this.props;
 
+		const {
+			lengthOfList,
+		} = this.state;
+
 		return (
 			<div
 				style={styles.container}
 			>
 				{
-					data.map((post, i) => {
+					data.slice(0, lengthOfList).map((post, i) => {
 						const targetPost = (match.path.split('/').map((e) => {
 							if(e === 'gallery' || e === 'Gallery') {
 								return;
